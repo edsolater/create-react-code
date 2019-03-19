ac2480c039a7e01365287a4ac93d38c225215d31
edsolater <edsolater@qq.com>
2019-03-19 12:54:34 +0800

    add styled-components

--- template-component.js
+++ template-component.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import styled from 'styled-components'
 
 /* placeholder: import selectors */
 /* placeholder: import actionCreators */
 /* placeholder: import related components */
 
+const Wrapper = styled.div``
+
 const $TM_FILENAME_BASE = ({/* placeholder: get mapState Props *//* placeholder: get mapDispatch Props */}) => {
   return (
-    <div className="$TM_FILENAME_BASE">
+    <Wrapper>
       <span>this is $TM_FILENAME_BASE</span>
       {/* placeholder: use related components */}
-    </div>
+    </Wrapper>
   )
 }
 /* placeholder: set mapState with selectors */
